a69ac467f6ded833d250be0597cffbe43e31b738
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-02-07 17:03:34 +0100

    Add functions to generate memes. Still need to add download function.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function App() {
   // Set intial states
   const [topText, setTopText] = useState('');
   const [bottomText, setBottomText] = useState('');
-  const [template, setTemplate] = useState('doge');
-  const [memeURL, setURL] = useState(
-    'https://api.memegen.link/images/preview.jpg?template=doge&lines[]=create+a&lines[]=meme',
-  );
+  const [memeTemplate, setMemeTemplate] = useState('');
+
+  // Define the meme URL based on user input
+  const memeURL = () => {
+    if (!topText && !bottomText) {
+      return 'https://api.memegen.link/images/doge/create a/meme.png';
+    } else {
+      return `https://api.memegen.link/images/${memeTemplate}/${topText}/${bottomText}.png`;
+    }
+  };
+
+  // Download image from URL
+  const downloadImage = () => {};
 
   return (
     <div className="memeGenerator">
@@ -16,7 +25,14 @@ export default function App() {
 
       <label>
         Top Text <br />
-        <input name="topText" placeholder="Add top text" value={topText} />
+        <input
+          name="topText"
+          placeholder="Add top text"
+          value={topText}
+          onChange={(event) => {
+            setTopText(event.currentTarget.value);
+          }}
+        />
       </label>
 
       <label>
@@ -25,6 +41,9 @@ export default function App() {
           name="bottomText"
           placeholder="Add bottom text"
           value={bottomText}
+          onChange={(event) => {
+            setBottomText(event.currentTarget.value);
+          }}
         />
       </label>
 
@@ -33,15 +52,22 @@ export default function App() {
         <input
           name="memeTemplate"
           placeholder="Choose a template"
-          value={template}
+          value={memeTemplate}
+          onChange={(event) => {
+            setMemeTemplate(event.currentTarget.value);
+          }}
         />
       </label>
+
       <h2>Your meme:</h2>
+
       <img
-        src={memeURL}
+        src={memeURL(memeTemplate, topText, bottomText)}
         alt="meme template preview"
         data-test-id="meme-image"
       />
+
+      <button>Download</button>
     </div>
   );
 }
