f5a74f8a55723c1a6aedcd3585bdad8942e9b14d
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-02-07 11:59:15 +0100

    Set up intial page, and add styling.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import './App.css';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  // Set intial states
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [template, setTemplate] = useState('doge');
+  const [memeURL, setURL] = useState(
+    'https://api.memegen.link/images/preview.jpg?template=doge&lines[]=create+a&lines[]=meme',
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="memeGenerator">
+      <h1>Meme Generator</h1>
+
+      <label>
+        Top Text <br />
+        <input name="topText" placeholder="Add top text" value={topText} />
+      </label>
+
+      <label>
+        Bottom text <br />
+        <input
+          name="bottomText"
+          placeholder="Add bottom text"
+          value={bottomText}
+        />
+      </label>
+
+      <label>
+        Meme template <br />
+        <input
+          name="memeTemplate"
+          placeholder="Choose a template"
+          value={template}
+        />
+      </label>
+      <h2>Your meme:</h2>
+      <img
+        src={memeURL}
+        alt="meme template preview"
+        data-test-id="meme-image"
+      />
     </div>
   );
 }
